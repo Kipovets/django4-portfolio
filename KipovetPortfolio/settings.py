a7f4e0bd71671c2c53bd4d7d80e54514778e30e6
Kipovets <108383664+Kipovets@users.noreply.github.com>
2022-08-11 09:43:27 +0300

    Update settings.py

--- KipovetPortfolio/settings.py
+++ KipovetPortfolio/settings.py
@@ -19,11 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-u-9bz90&x7oq9uz)vk*f6676rprvl+h9*g8yy!-fcvxdsf+*n9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
